f9a8e52f6993e5c2f473f507651e52456bf5d150
tangzhi <948144756@qq.com>
2026-01-22 14:41:37 +0800

    删除无用功能

--- demo-general/myNewlowcode/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: 'eslint-config-ali/typescript/react',
-  rules: {
-    'max-len': ['error', { code: 200 }],
-    'comma-dangle': 0,
-  },
-};
--- demo-general/myNewlowcode/tests/basic.test.ts
+++
@@ -1,4 +0,0 @@
-test('basic functions should be ok', () => {
-  // 这里放一些单元测试
-  expect(0).toBe(0);
-});
